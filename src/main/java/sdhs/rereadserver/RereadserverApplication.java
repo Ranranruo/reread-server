83f661d9b7b9d6f577923f8101eba5c302f2434b
sosegadamente <uijuncoding@gmail.com>
2024-07-17 19:42:36 +0900

    first commit

---
+++ src/main/java/sdhs/rereadserver/RereadserverApplication.java
@@ -0,0 +1,13 @@
+package sdhs.rereadserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RereadserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RereadserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sdhs/rereadserver/RereadserverApplicationTests.java
@@ -0,0 +1,13 @@
+package sdhs.rereadserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RereadserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
